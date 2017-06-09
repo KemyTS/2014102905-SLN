d8b611d2b84401762880286ef4938b3329508015
KemyTS <kemy_ts@hotmail.com>
2017-06-09 18:59:04 -0500

    tercer commit

---
+++ 2014102905/2014102905-ENT/DTO/TipoViajeDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _2014102905_API.DTO
+{
+    public class TipoViajeDTO
+    {
+        public int TipoViajeId { get; set; }
+        public string Nombre { get; set; }
+    }
+}
